9341c36d3fe027dab8f7730617e4210269e29989
Victor Isai <lsc.visantana@gmail.com>
2017-11-23 10:58:12 -0700

    Se agregó la clase principal y la conexión a la base de datos

---
+++ BeerPOS/src/Connection/MySQL_Conexion.java
@@ -0,0 +1,24 @@
+/*
+ * Description : Esta clase realiza la conexion a la base de datos
+ * Date        : 24 November 2017
+ * Author      : Víctor Isaí Santana Machuca.
+ */
+
+package Connection;
+import java.sql.*;
+public class MySQL_Conexion {
+    
+    private static String dbName = "BeerPOS",
+                          URL = "localhost:3306", 
+                          User = "root",
+                          Password = "root";
+    
+    public static Connection getConnection() throws ClassNotFoundException, SQLException
+    {
+        Connection Response;
+        Class.forName("com.mysql.jdbc.Driver");
+        Response = DriverManager.getConnection("jdbc:mysql://" + URL + "/"+ dbName, User, Password);        
+        return Response;        
+    }
+    
+}
---
+++ BeerPOS/src/beerpos/BeerPOS.java
@@ -0,0 +1,14 @@
+/*
+ * Description  : Esta es la clase principal
+ * Author       : Víctor Isaí Santana Machuca
+ * Date         : 23/11/2017
+ */
+package beerpos;
+
+public class BeerPOS {
+
+    public static void main(String[] args) {
+        System.out.println("Esta es la clase principal");
+    }
+    
+}
