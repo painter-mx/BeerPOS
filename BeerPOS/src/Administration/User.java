3e3f59eebeb4e525ecc55d6c331423b27de52358
Victor Isai <lsc.visantana@gmail.com>
2017-11-24 17:56:06 -0700

    Se agregó la funcionalidad para dar de alta un usuario

--- BeerPOS/src/Administration/User.java
+++ BeerPOS/src/Administration/User.java
@@ -1,14 +1,65 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Description  :   This class contains User attributes
+ * Author       :   Víctor Isaí Santana Machuca
+ * Date         :   Nov 24, 2017.
  */
 package Administration;
+import Connection.MySQL_Conexion;
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-/**
- *
- * @author isai
- */
+
 public class User {
     
+    private String role;
+    private String user_name;
+    private String password;
+    
+    public User()
+    {
+        role = "";
+        user_name = "";
+        password = "";
+    }
+    
+    public boolean InsertUser()
+    {
+        boolean response = true;
+        try 
+        {
+            Connection Conexion = MySQL_Conexion.getConnection();
+            Statement Estancia = Conexion.createStatement();
+            Estancia.executeUpdate("INSERT INTO Users(role, user_name, password)"+
+                                  "VALUES('"+role+"','"+user_name+"',MD5('"+password+"'))");         
+        } catch (ClassNotFoundException | SQLException ex) {
+            response = false;
+            Logger.getLogger(User.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return response;
+    }
+    
+    public String getRole() {
+        return role;
+    }
+    
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getUser_name() {
+        return user_name;
+    }
+
+    public void setUser_name(String user_name) {
+        this.user_name = user_name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
